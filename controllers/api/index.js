e87b32f077f82be6376cf2d98bd9228ea3416db4
John Hysong <johnahysong@gmail.com>
2022-05-07 11:00:48 -0400

    worked on user model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,17 @@
 const { route } = require("../../../.Main/controllers/api");
-
 const router = require("express").Router();
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
 // Finish the required pathing for these variables
 // const userRoutes = ;
 // const postRoutes = ;
 // const commentRoutes = ;
 
-router.use("/user", userRoutes);
 // write the rest of the router.use routes
+router.use("/user", userRoutes);
+router.use("/post", postRoutes);
+router.use("/comment", commentRoutes);
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -4,6 +4,9 @@ const sequelize = require("../config/config");
 
 class User extends Model {
     // set up a method to run on instance data (per user) to check passwords
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
 };
 
 User.init(
@@ -16,17 +19,30 @@ User.init(
             autoIncrement: true
         },
         username: {
-
+            type: DataTypes.STRING,
+            allowNull: false,
         },
         password: {
-
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
         },
     },
     {
     hooks: {
         // set up beforeCreate lifecycle "hook"
         // beforeCreate: async () => {},
+        beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
         // beforeUpdate: async () => {}
+          beforeUpdate: async (updatedUserData) => {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          },
     },
     sequelize,
     timestamps: false,
