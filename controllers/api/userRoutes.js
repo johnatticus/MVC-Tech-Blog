68a4a39864600adcd2f48ba56106f64eb2d6d960
John Hysong <johnahysong@gmail.com>
2022-05-07 11:30:18 -0400

    did userroutes. probably wrong.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,14 +3,62 @@ const { User } = require("../../models");
 
 router.post("/", async (req, res) => {
     // Creating a new instance of user
+    try {
+        const userData = await User.create(req.body);
+    
+        req.session.save(() => {
+          req.session.user_id = userData.id;
+          req.session.logged_in = true;
+    
+          res.status(200).json(userData);
+        });
+      } catch (err) {
+        res.status(400).json(err);
+      }
 });
 
 router.post("/login", async (req, res) => {
     // User login
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+    
+        if (!userData) {
+          res
+            .status(400)
+            .json({ message: 'WRONG PASSWORD OR EMAIL...TRY AGAIN!!1!' });
+          return;
+        }
+    
+        const validPassword = await userData.checkPassword(req.body.password);
+    
+        if (!validPassword) {
+          res
+            .status(400)
+            .json({ message: 'WRONG PASSWORD OR EMAIL...TRY AGAIN!!1!' });
+          return;
+        }
+    
+        req.session.save(() => {
+          req.session.user_id = userData.id;
+          req.session.logged_in = true;
+          
+          res.json({ user: userData, message: 'LOGGED IN, GREAT JOB!' });
+        });
+    
+      } catch (err) {
+        res.status(400).json(err);
+      }
 });
 
 router.post("/logout", async (req, res) => {
     // User logout
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+          res.status(204).end();
+        });
+      } else {
+        res.status(404).end();
+      }
 });
 
 module.exports = router;
