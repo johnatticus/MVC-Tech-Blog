92ae25eb7c16a79431b867290460181ddedd200d
John Hysong <johnahysong@gmail.com>
2022-05-07 11:17:55 -0400

    worked on post and comment models

--- models/Comment.js
+++ models/Comment.js
@@ -5,6 +5,33 @@ class Comment extends Model {}
 
 Comment.init(
     // finish this Comment Model
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'User',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscore: true,
+        modelName: "Comment"
+        }
 );
 
 module.exports = Comment;
--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,47 @@
-// Finish the rest of this model
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/config");
+
+class Post extends Model {
+    // // set up a method to run on instance data (per user) to check passwords
+    // checkPassword(loginPw) {
+    //     return bcrypt.compareSync(loginPw, this.password);
+    //   }
+};
+
+Post.init(
+    { 
+        // Finish the user model
+        id: {
+            type: DataType.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'User',
+                key: 'id',
+            },
+        },
+    },
+    {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscore: true,
+    modelName: "Post"
+    }
+);
+
+module.exports = Post;// Finish the rest of this model
