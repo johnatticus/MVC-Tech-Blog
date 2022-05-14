f1c9f2d702b0838fa46f6d73b6aef287035e5401
John Hysong <johnahysong@gmail.com>
2022-05-14 01:38:35 -0400

    css changes. trying to get comment to work

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,11 +2,22 @@ const commentFormHandler = async function(event) {
     event.preventDefault();
   
     const postId = document.querySelector('input[name="post-id"]').value;
-    const body = document.querySelector('textarea[name="comment-body"]').value;
+    const content = document.querySelector('textarea[name="comment-body"]').value;
   
     // Create the functionality to help create the buttons for your website.
+    const response = await fetch(`/api/comment`, {
+      method: 'POST',
+      body: JSON.stringify({ post_id: postId, comment }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace(`/post/${postId}`);
+    } else {
+      alert('Failed to create comment');
   };
-  
+};
+
   document
     .querySelector('#new-comment-form')
     .addEventListener('submit', commentFormHandler);
