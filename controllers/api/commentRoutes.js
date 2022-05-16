7a45912a8fa369c02721171976d0233040c4018a
John Hysong <johnahysong@gmail.com>
2022-05-15 20:26:08 -0400

    trying to fix comments still

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,6 +2,32 @@ const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
+router.get("/", async (req, res) => {
+  // get comments
+  try {
+      const commentData = await Comment.findAll({
+        include: [
+          {
+            model: Comment,
+            attributes: ['comment'],
+          },
+        ],
+      });
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+// router.get("/", (req, res) => {
+//   Comment.findAll()
+//       .then((newComment) => res.json(newComment))
+//       .catch((err) => {
+//           console.log(err);
+//           res.status(500).json(err);
+//       });
+// });
+
 router.post("/", withAuth, async (req, res) => {
     // create a router to post comments to created posts
     try {
